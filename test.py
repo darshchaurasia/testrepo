4914e68873c02bb1a98edb0369876ff65868a549
Darsh Chaurasia <43368969+darshchaurasia@users.noreply.github.com>
2023-10-04 15:36:42 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+x = 2
+y = 5
+xy = 'Hey'
+print (x+y, xy)
+
+
+# In[4]:
+
+
+print("""My name is Darsh Chaurasia 
+I love Python.""")
+
+
+# In[5]:
+
+
+string1 = 'World'
+print('Hello', string1)
+
+string2 = '!'
+print('Hello', string1, string2)
+
+
+# In[6]:
+
+
+print('Hello' + string1 + string2)
+
+
+# In[7]:
+
+
+print("Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug")
+
+
+# In[8]:
+
+
+print("I want \\n to be printed.")
+
+
+# In[10]:
+
+
+x = 10
+print(type(x))
+ 
+s = 'abc'
+print(type(s))
+ 
+value= True
+print(type(value))
+
+percent=90.00
+print(type(percent))
+
+
+# In[ ]:
